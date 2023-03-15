b2d3e5d0d3cb42501c4fa5311795aa30138542f5
MKR <108106603+moonkangryul@users.noreply.github.com>
2023-03-15 21:21:41 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.09 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer ="";
+        StringBuffer rv = new StringBuffer(my_string);
+       return answer = rv.reverse().toString();
+    }
+}
