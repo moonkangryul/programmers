adc06e43bd11b1d59b220d63f039246d7089a4d0
MKR <108106603+moonkangryul@users.noreply.github.com>
2023-03-15 21:08:42 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.28 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        double a = (double)n/7;
+        return answer = (int)Math.ceil(a);
+    }
+}
